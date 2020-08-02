ddca4f160aaf3806104a506015240c58a74e3461
SergeyGrishchuk <sirgeyaid666@gmail.com>
2020-08-02 19:49:30 +0300

    HomeWork5

--- src/HomeWork5/Staffer.java
+++ src/HomeWork5/Staffer.java
@@ -1,5 +1,22 @@
 package HomeWork5;
 
 public class Staffer {
-    
+    String fullName;
+    String position;
+    String email;
+    int saluly;
+    int age;
+
+    public Staffer(String fullName, String position, String email, int saluly, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.saluly = saluly;
+        this.age = age;
+    }
+
+    public void info() {
+        System.out.printf("fullName: %s, Position: %s, Email: %s, Saluly: %,d, Age: %d\n",
+                            fullName, position, email, saluly, age);
+    }
 }
